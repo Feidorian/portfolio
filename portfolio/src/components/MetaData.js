48fd9c54f2a1a82db26ea258b6eafd6dd8bdffda
feidorian <brianmile10@yahoo.com>
2021-08-16 11:32:42 -0400

    update

--- portfolio/src/components/MetaData.js
+++ portfolio/src/components/MetaData.js
@@ -6,7 +6,7 @@ function MetaData() {
     return (
         <Box p={2} overflow='auto'>
         <Paper elevation='3' component={Stack} spacing={2} sx={{ maxWidth:480, mx:'auto', p: 2, borderRadius:'0.5rem'}}>
-            <Box>
+            {/* <Box>
                 <Typography fontSize='1.6rem' fontWeight='500' color='#1A237E' textAlign='center'>About</Typography>
                 <Typography textAlign='center'>
                     This website was developed as a react Application, built and deployed through github pages, and routed to the above domain: 
@@ -15,7 +15,7 @@ function MetaData() {
                 <Typography textAlign='center'>
                     The CSS implementation consists mostly of Material UI Components, with a few instances of  React-Bootstrap and Vanilla CSS.
                 </Typography>
-            </Box>
+            </Box> */}
 
             <Box>
                 <Typography fontSize='1.6rem' fontWeight='500' color='#1A237E' textAlign='center'>Credits</Typography>
